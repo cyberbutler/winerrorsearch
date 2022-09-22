49a17e5e43ba46f138df0a885d95df7fef2b4ed3
os-25928 <os-25928@osep.osep.citadel.local>
2022-09-22 15:53:50 -0400

    Added downloading of results to a local file to speed up future searching

--- winerrorsearch.py
+++ winerrorsearch.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import os
 import re
+import json
 import requests
 import argparse
 
+from rich.progress import Progress
 from rich.console import Console
 from rich.panel import Panel
 from rich.text import Text
@@ -10,6 +13,14 @@ from rich.text import Text
 REGEX_ERROR_CODE = re.compile(r'<p>(\d+)\s\((0x[A-F0-9]+)\)</p>')
 REGEX_ERROR_MSG = re.compile(r'\<span id="([A-Z_]+)"\>')
 REGEX_ERROR_DESC = re.compile(r'<p>([\w\s]+\.)</p>')
+
+
+def convertErrorCodesTupleToDict(errorcodetuple):
+    return [{
+            "message": e[0],
+            "code": list(e[1]),
+            "description": e[2]
+        } for e in errorcodetuple]
 
 
 def parseMSErrorCodeResponse(resp: requests.Response):
@@ -20,13 +31,7 @@ def parseMSErrorCodeResponse(resp: requests.Response):
     return zipped
 
 def findErrorCode(searchinput:str, searchtype: str, errorCodes: list=[]) -> list:
-    searchtypes = [
-        "message",
-        "code",
-        "description"
-    ]
-    idx = searchtypes.index(searchtype)
-    return list(filter(lambda e: searchinput in e[idx], errorCodes))
+    return list(filter(lambda e: searchinput in e[searchtype], errorCodes))
     
 
 microsoft_error_code_urls = [
@@ -49,16 +54,32 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Search for a Win32 Error Code")
     parser.add_argument('-t', '--type', help="The type of input you are searching for. Default: auto", choices=["message", "code", "description", "auto"], default="auto")
+    parser.add_argument('-s', '--save', help="Save the results to ~/.winerrorcodes.json and skip future requests to microsoft to retrieve the codes. Re-execute with this flag to redownload the results.", action="store_true", default=False)
     parser.add_argument('search_term', help="The term to search for, partial matches apply")
 
     args = parser.parse_args()
     arg_type = args.type
 
-    allErrors = []
+    if not os.path.exists(f"{os.path.expanduser('~')}/.winerrorcodes.json") or args.save:
+        console.print("[[green]+[/green]] Fetching results from microsoft") 
+        allErrorsTuple = []
+        with Progress() as progress:
+            task = progress.add_task("[green]Downloading:", total=len(microsoft_error_code_urls))
+            for url in microsoft_error_code_urls:
+                resp = requests.get(url)
+                allErrorsTuple += parseMSErrorCodeResponse(resp)
+                progress.advance(task)
+        
+        allErrors = convertErrorCodesTupleToDict(allErrorsTuple)
 
-    for url in microsoft_error_code_urls:
-        resp = requests.get(url)
-        allErrors += parseMSErrorCodeResponse(resp)
+        if args.save:
+            console.print("[[green]+[/green]] Saving to results ~/.winerrorcodes.json")
+            with open(f"{os.path.expanduser('~')}/.winerrorcodes.json", "w") as f:
+                f.write(json.dumps(allErrors))
+
+    else:
+        with open(f"{os.path.expanduser('~')}/.winerrorcodes.json", 'r') as f:
+            allErrors = json.load(f)
 
     if args.search_term.split('_')[0].isupper():
         arg_type = "message"
@@ -69,14 +90,14 @@ if __name__ == "__main__":
 
 
     for result in findErrorCode(args.search_term, arg_type, allErrors):
-        description = Text(result[2])
+        description = Text(result.get("description"))
         description.highlight_words([args.search_term], style="on blue")
 
 
         console.print(Panel(
             description, 
-            title=f"[bold][red]{result[0]}", 
-            subtitle=f"[yellow]{result[1][0]} [magenta]({result[1][1]})",
+            title=f"[bold][red]{result.get('message')}", 
+            subtitle=f"[yellow]{result.get('code')[0]} [magenta]({result.get('code')[1]})",
             padding=2,
             highlight=True,
             width=50,
